b3e463b5767fea72dea6c67f303c8ed2d76e8ea3
gerry-mandering <kimmen1119@gmail.com>
2023-03-31 20:48:53 +0900

    Feat: designing and coding moco group post page 1, 2

--- minseok/app/src/main/java/com/example/mocoapplication/LoginActivity.java
+++ minseok/app/src/main/java/com/example/mocoapplication/LoginActivity.java
@@ -1,14 +1,99 @@
 package com.example.mocoapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private TextInputEditText emailEdt, pwdEdt;
+    private Button loginBtn;
+    private ProgressBar loadingPB;
+    private TextView registerTV;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        emailEdt = findViewById(R.id.idEdtEmail);
+        pwdEdt = findViewById(R.id.idEdtPassword);
+        loginBtn = findViewById(R.id.idBtnLogin);
+        loadingPB = findViewById(R.id.idPBLoading);
+        registerTV = findViewById(R.id.idTVRegister);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        registerTV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loadingPB.setVisibility(View.VISIBLE);
+
+                Intent intent = new Intent(LoginActivity.this, RegistrationActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loadingPB.setVisibility(View.VISIBLE);
+
+                String email = emailEdt.getText().toString();
+                String pwd = pwdEdt.getText().toString();
+
+                if (TextUtils.isEmpty(email) && TextUtils.isEmpty(pwd)) {
+                    Toast.makeText(LoginActivity.this, "모든 항목을 입력해주십시오", Toast.LENGTH_SHORT).show();
+                    return;
+                } else {
+                    mAuth.signInWithEmailAndPassword(email, pwd).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                loadingPB.setVisibility(View.GONE);
+
+                                Toast.makeText(LoginActivity.this, "로그인 되었습니다", Toast.LENGTH_SHORT).show();
+
+                                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                startActivity(intent);
+                                finish();
+                            } else {
+                                loadingPB.setVisibility(View.GONE);
+                                Toast.makeText(LoginActivity.this, "로그인에 실패했습니다", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseUser user = mAuth.getCurrentUser();
+        if (user != null) {
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
+            this.finish();
+        }
     }
 }
