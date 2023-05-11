dee47d02cbcedb998d385c3e0d44b8dc8286d70c
gerry-mandering <kimmen1119@gmail.com>
2023-05-11 19:42:40 +0900

    Feat: adding a Map location logic

--- minseok/app/src/main/java/com/example/mocoapplication/GetMapLocationActivity.java
+++ minseok/app/src/main/java/com/example/mocoapplication/GetMapLocationActivity.java
@@ -1,18 +1,28 @@
 package com.example.mocoapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class GetMapLocationActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class GetMapLocationActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private TextView locationTV;
     private Button submitBtn;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +30,10 @@ public class GetMapLocationActivity extends AppCompatActivity {
 
         locationTV = findViewById(R.id.idTVLocation);
         submitBtn = findViewById(R.id.idBtnSubmit);
+
+//        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+//                .findFragmentById(R.id.map);
+//        mapFragment.getMapAsync(this);
 
         submitBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,5 +46,14 @@ public class GetMapLocationActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        LatLng school = new LatLng(37.379899, 126.928946);
+        googleMap.addMarker(new MarkerOptions()
+                .position(school)
+                .title("Marker in Sydney"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(school));
     }
 }
